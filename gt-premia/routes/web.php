491dd6e792baa7a26555add969ea7083db73e6b1
BrenoNAlmeida <brenonascialmeida@gmail.com>
2025-03-26 20:15:46 -0300

    fix: corrige configuraçao do spatie

--- gt-premia/routes/web.php
+++ gt-premia/routes/web.php
@@ -20,37 +20,45 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/premio', [PremioController::class, 'index'])->name('premio.index');
-    Route::get('/premio/create', [PremioController::class, 'create'])->name('premio.create');
-    Route::post('/premio/store', [PremioController::class, 'store'])->name('premio.store');
     Route::get('/premio/{premio}', [PremioController::class, 'show'])->name('premio.show');
-    Route::get('/premio/{premio}/edit', [PremioController::class, 'edit'])->name('premio.edit');
-    Route::put('/premio/{premio}', [PremioController::class, 'update'])->name('premio.update');
-    Route::delete('/premio/{premio}', [PremioController::class, 'destroy'])->name('premio.destroy');
-
     Route::post('premio/{premio}/solicitar_retirada', [PremioController::class, 'solicitar_retirada'])->name('premio.solicitar_retirada');
 
-    Route::get('/usuarios', [UserController::class, 'index'])->name('usuarios.index');
-    Route::post('/usuarios/store', [UserController::class, 'store'])->name('usuarios.store');
-    Route::get('/usuarios/create', [UserController::class, 'create'])->name('usuarios.create');
-    Route::get('/usuarios/{user}', [UserController::class, 'show'])->name('usuarios.show');
-    Route::get('/usuarios/{user}/edit', [UserController::class, 'edit'])->name('usuarios.edit');
-    Route::put('/usuarios/{user}', [UserController::class, 'update'])->name('usuarios.update');
-    Route::put('/usuarios/{user}/resetar_senha', [UserController::class, 'resetar_senha'])->name('usuarios.resetar_senha');
-    Route::delete('/usuarios/{user}', [UserController::class, 'destroy'])->name('usuarios.destroy');
-
-
-    //  transacao
     Route::get('/transacao', [TransacaoController::class, 'index'])->name('transacao.index');
+
     
-    Route::get('/transacao/create', [TransacaoController::class, 'create'])->name('transacao.create');
-    Route::post('/transacao', [TransacaoController::class, 'store'])->name('transacao.store');
-    Route::get('/transacao/{transacao}', [TransacaoController::class, 'show'])->name('transacao.show');
-    Route::get('/transacao/{transacao}/edit', [TransacaoController::class, 'edit'])->name('transacao.edit');
-    Route::put('/transacao/{transacao}', [TransacaoController::class, 'update'])->name('transacao.update');
-    Route::delete('/transacao/{transacao}', [TransacaoController::class, 'destroy'])->name('transacao.destroy');
-    Route::post('/transacao/{transacao}/aprovar', [TransacaoController::class, 'aprovar'])->name('transacao.aprovar');
-    Route::post('/transacao/{transacao}/reprovar', [TransacaoController::class, 'reprovar'])->name('transacao.reprovar');
+    Route::middleware(['role:rh|admin'])->group(function () { 
+        //  premio
+        Route::get('/premio/create', [PremioController::class, 'create'])->name('premio.create');
+        Route::post('/premio/store', [PremioController::class, 'store'])->name('premio.store');
+        Route::get('/premio/{premio}/edit', [PremioController::class, 'edit'])->name('premio.edit');
+        Route::put('/premio/{premio}', [PremioController::class, 'update'])->name('premio.update');
+        Route::delete('/premio/{premio}', [PremioController::class, 'destroy'])->name('premio.destroy');
+        
+
+        //  usuarios
+        Route::get('/usuarios', [UserController::class, 'index'])->name('usuarios.index');
+        Route::post('/usuarios/store', [UserController::class, 'store'])->name('usuarios.store');
+        Route::get('/usuarios/create', [UserController::class, 'create'])->name('usuarios.create');
+        Route::get('/usuarios/{user}', [UserController::class, 'show'])->name('usuarios.show');
+        Route::get('/usuarios/{user}/edit', [UserController::class, 'edit'])->name('usuarios.edit');
+        Route::put('/usuarios/{user}', [UserController::class, 'update'])->name('usuarios.update');
+        Route::put('/usuarios/{user}/resetar_senha', [UserController::class, 'resetar_senha'])->name('usuarios.resetar_senha');
+        Route::delete('/usuarios/{user}', [UserController::class, 'destroy'])->name('usuarios.destroy');
     
+        //  transacao
+        Route::get('/transacao/create', [TransacaoController::class, 'create'])->name('transacao.create');
+        Route::post('/transacao', [TransacaoController::class, 'store'])->name('transacao.store');
+        Route::get('/transacao/{transacao}', [TransacaoController::class, 'show'])->name('transacao.show');
+        Route::get('/transacao/{transacao}/edit', [TransacaoController::class, 'edit'])->name('transacao.edit');
+        Route::put('/transacao/{transacao}', [TransacaoController::class, 'update'])->name('transacao.update');
+        Route::delete('/transacao/{transacao}', [TransacaoController::class, 'destroy'])->name('transacao.destroy');
+        Route::post('/transacao/{transacao}/aprovar', [TransacaoController::class, 'aprovar'])->name('transacao.aprovar');
+        Route::post('/transacao/{transacao}/reprovar', [TransacaoController::class, 'reprovar'])->name('transacao.reprovar');
+        
+    });
+    
+
+
 });
 
 require __DIR__ . '/auth.php';
